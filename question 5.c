f5359bbcfcb7b9777802eaede4771deb97b12c08
ShyamModanwal <shyammodanwal705@gmail.com>
2024-05-25 09:13:40 +0530

    Add files via upload

---
+++ question 5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* 
+5. Write a program to print “Happy Teachers Day” on the screen 
+(Remember to print double and single quotes).
+*/
+// shyam modanwal 
+int main() {
+    printf("\"Happy Teachers Day\"\n");
+    return 0;
+}
+
+    
