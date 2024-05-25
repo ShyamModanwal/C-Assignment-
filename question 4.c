0c8ee85ee0e4f105e0a7863f6355e8f749f61dd2
ShyamModanwal <shyammodanwal705@gmail.com>
2024-05-25 09:13:15 +0530

    Add files via upload

---
+++ question 4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* 
+4. Write a program to print \n and \r on the screen.
+*/
+// shyam modanwal 
+int main() {
+    printf("\\n\n");
+    printf("\\r\n");
+    return 0;
+}
+
+    
